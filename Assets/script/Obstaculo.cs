d5a26fd3855374766d1c6e093e45e5c264d10292
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-05 13:46:58 -0300

    0.5 FATflap V0

--- Assets/script/Obstaculo.cs
+++ Assets/script/Obstaculo.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField]
     private float velocidade = 0.8f;
+    [SerializeField]
+    private float variacaoY;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY, variacaoY));
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+
+    void Destruir()
+    {
+        GameObject.Destroy(this.gameObject);
     }
 }
